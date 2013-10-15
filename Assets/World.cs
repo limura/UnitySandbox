4c7b66745720dd938251e865cdfc5d1e40cfb51d
IIMURA Takuji <uirou.j@gmail.com>
2013-10-15 09:26:24 +0900

    マウスでセルを追加したり削除したりできるようにした

--- Assets/World.cs
+++ Assets/World.cs
@@ -29,10 +29,16 @@ public class World : MonoBehaviour {
 	/// </summary>
   	GameObject[] m_CellMap = null;
 	
+	// cell の間隔
+	float cell_distance = 1.5f;
+	
+	// セレクタのGameObject
+	public GameObject selector = null;
+	
 	// 新しいcellを作って返します.
 	GameObject CreateNewCell(int x, int y, Quaternion prev_rotation)
 	{
-		GameObject obj = Instantiate(cellPrefab, new Vector3((x - width / 2.0f) * 1.5f, 0.0f, (y - height / 2.0f) * 1.5f), prev_rotation) as GameObject;
+		GameObject obj = Instantiate(cellPrefab, new Vector3((x - width / 2.0f) * cell_distance, 0.0f, (y - height / 2.0f) * cell_distance), prev_rotation) as GameObject;
 		return obj;
 	}
 	
@@ -112,10 +118,68 @@ public class World : MonoBehaviour {
       	return true;
   	}
 	
-	// cellの状態を１ターン分更新します.
-  	void CellUpdate()
-  	{
-		// 怪しく現在の回転状態を拾ってきます(後で新規に作成されるcellが同じ角度から生成できるようにするためです)
+	class Position
+	{
+		public int x = 0;
+		public int y = 0;
+	}
+	
+	// マウスがクリックされたなら SelectCollision の座標を取得します
+	Position GetMouseCellPos()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		int layerMask = 1 << 8; // 8 は SelectCollision のレイヤのはず……
+		if(!Physics.Raycast(ray, out hit, 200.0f, layerMask))
+		{
+			return null;
+		}
+		float x = hit.point.x;
+		float y = hit.point.z;
+		if(x < (-1 * width / 2.0f * cell_distance) || x > width / 2.0f * cell_distance
+			|| y < (-1 * height / 2.0f * cell_distance) || y > height / 2.0f * cell_distance)
+		{
+			return null;
+		}
+		Position pos = new Position();
+		pos.x = (int)(x / cell_distance + width / 2.0f + cell_distance / 2.0f);
+		pos.y = (int)(y / cell_distance + width / 2.0f + cell_distance / 2.0f);
+		return pos;
+	}
+	
+	void UpdateMousePosCell()
+	{
+		Position pos = GetMouseCellPos();
+		if(pos == null)
+		{
+			selector.SetActive(false);
+			return;
+		}
+		selector.transform.position = new Vector3((pos.x - width / 2.0f) * cell_distance, 0.0f, (pos.y - height / 2.0f) * cell_distance);
+		selector.SetActive(true);
+		
+		Quaternion prev_rotation = GetCellRotation();
+		int n = pos.x + pos.y * width;
+		if(Input.GetMouseButton (0))
+		{
+			if(m_CellMap[n] == null)
+			{
+				m_CellMap[n] = CreateNewCell(pos.x, pos.y, prev_rotation);
+			}
+		}
+		if(Input.GetMouseButton (1))
+		{
+			if(m_CellMap[n] != null)
+			{
+				DestroyCell(m_CellMap, pos.x, pos.y);
+				m_CellMap[n] = null;
+			}
+		}
+	}
+	
+	// 怪しく現在の回転状態を拾ってきます(後で新規に作成されるcellが同じ角度から生成できるようにするためです)
+	Quaternion GetCellRotation()
+	{
 		Quaternion prev_rotation = Quaternion.identity;
 		foreach(GameObject obj in m_CellMap)
 		{
@@ -125,7 +189,13 @@ public class World : MonoBehaviour {
 				break;
 			}
 		}
-
+		return prev_rotation;
+	}
+	
+	// cellの状態を１ターン分更新します.
+  	void CellUpdate()
+  	{
+		Quaternion prev_rotation = GetCellRotation();
 		// 新しく GameObject[] を作ってそちらに新しい状態を生成します.
       	GameObject[] newCellMap = new GameObject[width * height];
       	for(int n = 0; n < newCellMap.Length; n++)
@@ -150,6 +220,7 @@ public class World : MonoBehaviour {
               	newCellMap[n] = null;
           	}
         }
+		
       	m_CellMap = newCellMap;
     }
 
@@ -160,5 +231,6 @@ public class World : MonoBehaviour {
         {
         	CellUpdate();
         }
+		UpdateMousePosCell();
 	}
 }
